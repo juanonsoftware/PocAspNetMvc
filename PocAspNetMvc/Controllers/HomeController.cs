a0ceb47ad235fe401bbcf976d39535d5ada8add5
juanonsoftware <juanonsoftware@mail.com>
2022-10-11 12:12:14 +0700

    Adding/Removing items with autocomplete

--- PocAspNetMvc/Controllers/HomeController.cs
+++ PocAspNetMvc/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 
 namespace PocAspNetMvc.Controllers
@@ -42,33 +41,6 @@ namespace PocAspNetMvc.Controllers
         public ActionResult TypeaheadJs()
         {
             return View();
-        }
-    }
-
-    public static class SampleDataForAutoComplete
-    {
-        public static IDictionary<int, string> _items;
-
-        public static IDictionary<int, string> InitValues()
-        {
-            if (_items == null)
-            {
-                _items = new Dictionary<int, string>
-                {
-                    { 1, "Docker"},
-                    { 2, "Google Cloud Platform" },
-                    { 3, "Amazone" },
-                    { 4, "Azure" },
-                    { 5, "Alibaba Cloud" },
-                    { 6, "Oracle" },
-                    { 7, "Vault" },
-                    { 71, "VMWare" },
-                    { 72, "HyperV Systems" },
-                    { 73, "Pentalog" },
-                    { 74, "Microsoft" },
-                };
-            }
-            return _items;
         }
     }
 }
---
+++ PocAspNetMvc/Controllers/SampleDataForAutoComplete.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace PocAspNetMvc.Controllers
+{
+    public static class SampleDataForAutoComplete
+    {
+        public static IDictionary<int, string> _items;
+
+        public static IDictionary<int, string> InitValues()
+        {
+            if (_items == null)
+            {
+                _items = new Dictionary<int, string>
+                {
+                    { 1, "Docker"},
+                    { 2, "Google Cloud Platform" },
+                    { 3, "Amazone" },
+                    { 4, "Azure" },
+                    { 5, "Alibaba Cloud" },
+                    { 6, "Oracle" },
+                    { 7, "Vault" },
+                    { 71, "VMWare" },
+                    { 72, "HyperV Systems" },
+                    { 73, "Pentalog" },
+                    { 74, "Microsoft" },
+                };
+            }
+            return _items;
+        }
+    }
+}
