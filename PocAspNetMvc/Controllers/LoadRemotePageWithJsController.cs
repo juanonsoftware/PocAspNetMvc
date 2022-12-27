5f1a51a06754d8c559b8906923c16e10a70d98c7
juanonsoftware <juanonsoftware@mail.com>
2022-12-27 09:33:10 +0700

    Add LoadRemotePage with JavaScript

---
+++ PocAspNetMvc/Controllers/LoadRemotePageWithJsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PocAspNetMvc.Controllers
+{
+    public class LoadRemotePageWithJsController : Controller
+    {
+        public ActionResult LoadRemotePage()
+        {
+            return View();
+        }
+    }
+}
