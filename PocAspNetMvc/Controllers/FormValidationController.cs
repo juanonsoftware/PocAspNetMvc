de5e73ab2ba4ef9a1640e8725801df6a72e3d567
juanonsoftware <juanonsoftware@mail.com>
2022-12-30 11:07:13 +0700

    Form Validation

---
+++ PocAspNetMvc/Controllers/FormValidationController.cs
@@ -0,0 +1,25 @@
+﻿using PocAspNetMvc.Models;
+using System.Web.Mvc;
+
+namespace PocAspNetMvc.Controllers
+{
+    public class FormValidationController : Controller
+    {
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            return View(new ProductViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return Redirect("~/");
+        }
+    }
+}
---
+++ PocAspNetMvc/Models/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PocAspNetMvc.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace PocAspNetMvc.Models
+{
+    public class ProductViewModel
+    {
+        [Required(ErrorMessageResourceType = typeof(FormValidationResx), ErrorMessageResourceName = nameof(FormValidationResx.Required))]
+        [Display(ResourceType = typeof(FormValidationResx), Name = nameof(Name))]
+        [MinLength(5, ErrorMessageResourceType = typeof(FormValidationResx), ErrorMessageResourceName = nameof(FormValidationResx.MinLength))]
+        [MaxLength(20, ErrorMessageResourceType = typeof(FormValidationResx), ErrorMessageResourceName = nameof(FormValidationResx.MaxLength))]
+        public string Name { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(FormValidationResx), ErrorMessageResourceName = nameof(FormValidationResx.Required))]
+        [Display(ResourceType = typeof(FormValidationResx), Name = nameof(Details))]
+        public string Details { get; set; }
+
+        [Display(ResourceType = typeof(FormValidationResx), Name = nameof(Price))]
+        public int Price { get; set; }
+    }
+}
